0dc025682de881a0a67e5f77fc8796e34b065ea5
Tim Culverhouse <tim@timculverhouse.com>
2025-12-05 15:45:14 +0200

    add HEAD install option

--- Formula/prise.rb
+++ Formula/prise.rb
@@ -4,6 +4,7 @@ class Prise < Formula
   url "https://github.com/rockorager/prise/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
+  head "https://github.com/rockorager/prise.git", branch: "main"
 
   depends_on "zig" => :build
 
