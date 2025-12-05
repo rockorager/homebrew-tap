1bc0dca939660b4e11c956760c50579cb7c19d1d
Tim Culverhouse <tim@timculverhouse.com>
2025-12-05 17:50:54 +0200

    Fix prise checksum

--- Formula/prise.rb
+++ Formula/prise.rb
@@ -2,7 +2,7 @@ class Prise < Formula
   desc "Terminal multiplexer targeted at modern terminals"
   homepage "https://github.com/rockorager/prise"
   url "https://github.com/rockorager/prise/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "5845a8fa440a6b827d9b20507da2c4bb3e382bce5dcbaf11307fc148599d8135"
   license "MIT"
   head "https://github.com/rockorager/prise.git", branch: "main"
 
